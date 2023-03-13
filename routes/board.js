a4f500213d3ff8382fecc5de0d86c839723d38e8
성희 <86584041+tjdgml97@users.noreply.github.com>
2023-03-13 13:27:43 +0900

    [#2] : 글 쓰기페이지  이동,글  추가

--- routes/board.js
+++ routes/board.js
@@ -14,8 +14,16 @@ const ARTICLE = [
 ];
 
 // localhost:4000/board/
+// 글 전체 목록 보여주기 
 router.get('/',(req,res)=> {
   res.render('board',{ARTICLE , articleCounts: ARTICLE.length})
   // ARTICLE : ARTICLE ( 키에 배열을 담는것 )
 })
+
+// 글 쓰기 모드로 이동
+router.get('/write',(req,res) => {});
+
+// 글 추가 
+router.post('/wirte',(req,res)=>{});
+
 module.exports = router;
