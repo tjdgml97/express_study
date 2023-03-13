14f0454020dcebb8774423d1c02ebbc8f4dadf15
성희 <86584041+tjdgml97@users.noreply.github.com>
2023-03-13 13:14:50 +0900

    [#2] : 게시글 데이터 추가

--- routes/board.js
+++ routes/board.js
@@ -2,4 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const ARTICLE = [
+  {
+    title: 'title1',
+    content: 'hello'
+  },
+  {
+    title: 'title2',
+    content: 'hello2'
+  },
+];
+
 module.exports = router;
