2a1963e818cde9a258e9ebaff90da0d696cbf59d
성희 <86584041+tjdgml97@users.noreply.github.com>
2023-03-17 11:10:17 +0900

    [#3]: 쿠키 발행 실습 추가

--- routes/cookie.js
+++ routes/cookie.js
@@ -3,12 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=> {
+  res.render('cookie');
+});
+
+router.get('/cook',(req,res) => {
   res.cookie('alert', true, {
-    expires : new Date(Date.now() + 1000 * 60),
-    httpOnly : true, 
+    //옵션 설정 
+    maxAge : 1000 * 5, 
+    httpOnly : false,
   });
+  res.status(200).json('쿠키 굽기 성공!');
   console.log(req.cookies);
-  res.render('index');
+  res.send('cookie');
 });
 
 module.exports = router;
