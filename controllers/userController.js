1c4db0e53b557a3e68f6a49453630f35d0e557ff
성희 <86584041+tjdgml97@users.noreply.github.com>
2023-03-22 09:51:47 +0900

    [#6] : 회원가입  메세지 수정 , id 값 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,37 @@
 const connection = require('./dbConnect');
 const mongoClient = require('./mongoConnect');
 
+const UNEXPECTED_MSG = 
+  '알 수 없는 문제 발생<br><a href="/register">회원 가입으로 이동<a>';
+
+const DUPLICATED_MSG = 
+  '동일한 ID를 가지는 회원이 존재합니다.<br><a href="/register">회원 가입으로 이동<a>';
+
+const SUCCESS_MSG = 
+'회원가입 성공!.<br><a href="/register">로그인으로 이동<a>';
+
+
+
+const registerUser = async (req, res) => {
+  try{
+    const client = await mongoClient.connect();
+    const user = client.db('kdt5').collection('user');
+
+    const duplicatedUser = await user.findOne({ id: req.body.id});
+    if(duplicatedUser) return res.status(400).send(DUPLICATED_MSG);
+
+    await user.insertOne(req.body);
+    res.status(200).send(SUCCESS_MSG)
+  }catch(err) {
+    console.error(err);
+    res.status(500).send(UNEXPECTED_MSG,);
+  }
+}
+
+module.exports = {
+  registerUser,
+};
+
 const userDB = {
 
     // 중복회원찾기
